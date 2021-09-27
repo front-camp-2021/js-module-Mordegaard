efe40e16af3413d5c1d577b0060b2eeb8531978a
Mordegaard <bodme12@gmail.com>
2021-09-27 15:29:02 +0300

    three tasks of js-module

--- src/module-1/weirdString/index.js
+++ src/module-1/weirdString/index.js
@@ -1,1 +1,14 @@
-export const weirdString = (str = "") => {};
+export const weirdString = (str = "") => {
+  if (str) {
+    let words = str.split(' ');
+    words = words.map(word=>{
+      word = word.toUpperCase();
+      let lastLetter = word[word.length-1].toLowerCase();
+      let letters = word.split('');
+      letters[letters.length-1] = lastLetter;
+      word = letters.join('');
+      return word;
+    });
+    return words.join(' ');
+  } else return "";
+};
