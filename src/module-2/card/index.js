461882cf8dc63c8dec3aef07f6a3b386de2bca6e
Mordegaard <bodme12@gmail.com>
2021-09-27 18:10:19 +0300

    module-2

--- src/module-2/card/index.js
+++ src/module-2/card/index.js
@@ -22,6 +22,42 @@ export default class Card {
   }
 
   render () {
-    // ... your logic
+    let card = document.createElement("div");
+    card.classList.add("product-card");
+    card.innerHTML = `
+    <div class="product-top-section">
+        <div class="product-image-container flexed">
+          <img class="product-image" src="${this.images[0]}">
+        </div>
+        <div class="rating-price-row">
+          <div class="product-rating">
+            <span>${this.rating}</span>
+            <svg viewBox="0 0 576 512" fill="currentColor">
+              <use xlink:href="icons.svg#star"></use>
+            </svg>
+          </div>
+          <span class="product-price">$${this.price}</span>
+        </div>
+        <div class="product-description-container">
+          <h3 class="product-title">${this.title}</h3>
+          <span class="product-description">${this.category} ${this.brand}</span>
+        </div>
+      </div>
+      <div class="product-buttons">
+        <button class="wishlist-btn flexed">
+          <svg viewBox="0 0 512 512" fill="currentColor">
+            <use xlink:href="icons.svg#heart"></use>
+          </svg>
+          <span>Wishlist</span>
+        </button>
+        <button class="cart-btn flexed">
+          <svg viewBox="0 0 448 512" fill="currentColor">
+            <use xlink:href="icons.svg#cart"></use>
+          </svg>
+          <span>Add to cart</span>
+        </button>
+      </div>
+    `;
+    this.element = card;
   }
 }
