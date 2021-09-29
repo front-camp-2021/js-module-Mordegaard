47f062a19c7d0b900bb70183634d1d630f0010cc
Mordegaard <bodme12@gmail.com>
2021-09-29 15:15:07 +0300

    add debounce

--- src/module-1/debounce/index.js
+++ src/module-1/debounce/index.js
@@ -1,5 +1,15 @@
-export const debounce = (fn, delay = 0) => {
-
+export debounce = (...args) => {
+  let [fn, delay] = args;
+  let cooldown = args.length > 1 && args[1] > 0 ? args[1] : 0;
+  return function() {
+    let fnArgs = [...arguments];
+    if (!fn.cd) {
+      fn.cd = true;
+      timeout = setTimeout(()=>{
+        fn.cd = false;
+        clearTimeout(timeout);
+      }, cooldown);
+      return fn.apply(this, fnArgs);
+    }
+  }
 }
-
-
